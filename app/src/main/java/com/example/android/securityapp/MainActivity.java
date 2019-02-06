b841c6e180dc2c7c5262b8983fb423c4ef1c42a7
vsaisujeeth <vsaisujeeth10@yahoo.com>
2019-02-06 16:41:43 +0530

    First time entry

---
+++ app/src/main/java/com/example/android/securityapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.securityapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void signInButtons(View view) {
+        if(view.getId() == R.id.button_admin){
+
+        }
+        else if(view.getId() == R.id.button_res){
+
+        }
+        else if(view.getId() == R.id.button_sec){
+
+            Intent intent = new Intent(this,Security.class);
+            startActivity(intent);
+
+        }
+    }
+}
