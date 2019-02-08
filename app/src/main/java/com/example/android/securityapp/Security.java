3385171c8c5240bfca3e24b5553ae267a65a9125
vsaisujeeth <vsaisujeeth10@yahoo.com>
2019-02-08 15:10:18 +0530

    Added photo capture and created second ui

--- app/src/main/java/com/example/android/securityapp/Security.java
+++ app/src/main/java/com/example/android/securityapp/Security.java
@@ -1,13 +1,43 @@
 package com.example.android.securityapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import org.w3c.dom.Text;
 
 public class Security extends AppCompatActivity {
 
+
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security);
+
+        Button camera = (Button)findViewById(R.id.but_clickPhoto);
+        EditText text = (EditText)findViewById(R.id.editText_regno);
+        imageView = (ImageView)findViewById(R.id.imageView2);
+
+
+    }
+
+    public void Shoot(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
